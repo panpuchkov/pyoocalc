12714d373a8d7bebb627fe8f3100c246b8889091
Юрий Пучков <purple@amhost.net>
2015-12-08 23:09:09 +0200

    Devel

--- clear.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-PRJ_PATH="./"
-
-echo -n "Removing '*.pyc' files          ":
-find "$PRJ_PATH" -iname *.pyc -exec rm {} \; && echo " - OK";
-echo -n "Removing '*~' files          ":
-find "$PRJ_PATH" -iname *~ -exec rm {} \; && echo " - OK";
--- doc_gen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "Generation AM4-Server side (python) API documentation"
-cd src/
-#epydoc --html * -o ../../src/arilot/docs/m/api/
-epydoc --html * -o ../doc/tmlibra
-cd ..
