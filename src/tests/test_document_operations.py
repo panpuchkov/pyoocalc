884012f4283430db100a5c3a2b86c521d32d345e
Yurii Puchkov <panpuchkov@gmail.com>
2015-12-13 18:01:42 +0200

    Release

--- src/tests/test_document_operations.py
+++ src/tests/test_document_operations.py
@@ -12,54 +12,64 @@ import pyloo
 ###############################################################################
 
 
+def pyloo_open_close_doc(f):
+    """
+    Decorator. Opens libre/open office document call function for testing
+    and closes the document.
+    """
+
+    def _f(*args, **kwargs):
+        doc = pyloo.Document()
+        file_name = os.getcwd() + "/test.ods"
+        doc.open_document(file_name)
+
+        kwargs['doc'] = doc
+        _f._retval = f(*args, **kwargs)
+
+        doc.close_document()
+    return _f
+
+###############################################################################
+
+
 class Test_PyLOO_Document(unittest.TestCase):
 
     def test_document_initialize(self):
         doc = pyloo.Document()
         self.assertFalse(doc.is_null())
 
-    def test_document_open_close(self):
-        doc = pyloo.Document()
-        file_name = os.getcwd() + "/test.ods"
-        self.assertTrue(doc.open_document(file_name), "OPEN")
-        self.assertTrue(doc.close_document(), "CLOSE")
-
-    def test_document_new_close(self):
-        doc = pyloo.Document()
-        self.assertTrue(doc.new_document(), "NEW")
-        self.assertTrue(doc.close_document(), "CLOSE")
-
     def test_document_new_save_close(self):
         doc = pyloo.Document()
         file_name_saved = os.getcwd() + "/test_saved.ods"
-        self.assertTrue(doc.new_document(), "OPEN")
-        self.assertTrue(doc.save_document(file_name_saved), "SAVE")
-        self.assertTrue(os.path.isfile(file_name_saved), "Is file exists")
+        self.assertTrue(doc.new_document())
+        self.assertTrue(doc.save_document(file_name_saved))
+        self.assertTrue(os.path.isfile(file_name_saved),
+                        "File does not exists")
+        # remove test file
         os.remove(file_name_saved)
-        self.assertTrue(doc.close_document(), "CLOSE")
+        self.assertTrue(doc.close_document())
 
-    def test_document_sheets(self):
+    def test_document_open_close(self):
         doc = pyloo.Document()
         file_name = os.getcwd() + "/test.ods"
-        doc.open_document(file_name)
-        sheets = doc.sheets()
-        self.assertFalse(sheets.is_null(), "get sheets object")
-        doc.close_document()
+        self.assertTrue(doc.open_document(file_name))
+        self.assertTrue(doc.close_document())
 
-    def test_document_fields(self):
-        doc = pyloo.Document()
-        file_name = os.getcwd() + "/test.ods"
-        doc.open_document(file_name)
-        fields = doc.fields()
-        self.assertFalse(fields.is_null(), "get fields object")
-        doc.close_document()
+    @pyloo_open_close_doc
+    def test_document_sheets(self, doc):
+        self.assertFalse(doc.sheets().is_null(), "get sheets object")
+
+    @pyloo_open_close_doc
+    def test_document_fields(self, doc):
+        self.assertFalse(doc.fields().is_null(), "get fields object")
 
 ###############################################################################
 
 
 class Test_PyLOO_Fields(unittest.TestCase):
 
-    def test_fields_field(self):
+    @pyloo_open_close_doc
+    def test_fields_field(self, doc):
         doc = pyloo.Document()
         file_name = os.getcwd() + "/test.ods"
         doc.open_document(file_name)
@@ -67,7 +77,8 @@ class Test_PyLOO_Fields(unittest.TestCase):
         self.assertFalse(field.is_null(), "get field object")
         doc.close_document()
 
-    def test_fields_count(self):
+    @pyloo_open_close_doc
+    def test_fields_count(self, doc):
         doc = pyloo.Document()
         file_name = os.getcwd() + "/test.ods"
         doc.open_document(file_name)
@@ -79,29 +90,19 @@ class Test_PyLOO_Fields(unittest.TestCase):
 
 class Test_PyLOO_Field(unittest.TestCase):
 
-    def test_field_set_get(self):
-        doc = pyloo.Document()
-        file_name = os.getcwd() + "/test.ods"
-        doc.open_document(file_name)
+    @pyloo_open_close_doc
+    def test_field_set_get(self, doc):
         field = doc.fields().field("TABLE_NAME")
-
         test_value = "Test table name"
         self.assertTrue(field.set_value(test_value))
         self.assertEqual(field.value(), test_value)
 
-        doc.close_document()
-
-    def test_field_insert_rows(self):
-        doc = pyloo.Document()
-        file_name = os.getcwd() + "/test.ods"
-        doc.open_document(file_name)
+    @pyloo_open_close_doc
+    def test_field_insert_rows(self, doc):
         field = doc.fields().field("TABLE_NAME")
-
         test_value = "Test table name"
         self.assertTrue(field.set_value(test_value))
         self.assertEqual(field.value(), test_value)
-
-        doc.close_document()
 
 ###############################################################################
 
