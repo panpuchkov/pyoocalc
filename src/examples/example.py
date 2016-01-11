32c91140399a6091f6e093a79e834dcf4bfdba38
Yurii Puchkov <panpuchkov@gmail.com>
2016-01-11 19:11:47 +0200

    Release

--- src/examples/example.py
+++ src/examples/example.py
@@ -25,33 +25,27 @@ import sys
 sys.path.append('./../')
 import pyoocalc
 
-with pyoocalc.Document(autostart=True) as doc:
-    print ("PyOOCalc version: ", doc.version)
-    file_name = os.getcwd() + "/example.ods"
-    doc.open_document(file_name)
-
-
-exit()
-
 # open document
 doc = None
 try:
-#     doc = pyoocalc.Document(autostart=True)
-    with pyoocalc.Document(autostart=True) as doc:
-        print ("PyOOCalc version: ", doc.version)
+    doc = pyoocalc.Document(autostart=True)
+    print ("PyOOCalc version: ", doc.version)
 except OSError as e:
     print(e.errno, e.strerror)
 except pyoocalc.NoConnectException as e:
     print ("Error: The OpenOffice.org process is not started or \
-does not listen on the resource (" + e.Message + ")\n\
+does not listen on the resource.\n\
+{0}\n\n\
 Start LibreOffice/OpenOffice in listening mode, \
 example:\n\
     soffice \
---accept=\"socket,host=localhost,port=2002;urp;\"\n")
+--accept=\"socket,host=localhost,port=2002;urp;\"\n".format(e.Message)
+        )
 
 if doc:
     file_name = os.getcwd() + "/example.ods"
     file_name_saved = os.getcwd() + "/example_saved.ods"
+    # Open document
     doc.open_document(file_name)
 
     # Get document fields
@@ -60,39 +54,44 @@ if doc:
 
     # Get field "HEADER"
     field = fields.field("HEADER")
-    print ("Document header is: " + str(field.is_null))
+    print ("Document header is: ", str(field.is_null))
 
     # Set values
     field = fields.field("TABLE_NAME")
     field.set_value("Test table name")
-    print ("New table name is: " + field.value())
+    print ("New table name is: ", field.value())
 
-    # Insert rows
+    ########################################
+    # Get table column fields
     field1 = fields.field("FIELD_1")
     field2 = fields.field("FIELD_2")
     field3 = fields.field("FIELD_3")
     field4 = fields.field("FIELD_4")
 
+    # Set number of rows and step
     num_rows = 5
     step = 2
+    # Insert rows into the table
     if num_rows > 0:
         field1.insert_rows(num_rows=num_rows-1, step=step, columns_to_copy=200)
+    # Insert data into the cells by field name and offset
     for i in range(1, num_rows + 1):
-        field1.set_value("F1." + str(i), 0, i * step - (step - 1))
-        field2.set_value("F2." + str(i), 0, i * step - (step - 1))
-        field3.set_value("F3." + str(i), 0, i * step - (step - 1))
-        field4.set_value("F4." + str(i), 0, i * step - (step - 1))
+        field1.set_value("F1.{0}".format(str(i)), 0, i * step - (step - 1))
+        field2.set_value("F2.{0}".format(str(i)), 0, i * step - (step - 1))
+        field3.set_value("F3.{0}".format(str(i)), 0, i * step - (step - 1))
+        field4.set_value("F4.{0}".format(str(i)), 0, i * step - (step - 1))
 
+    # Insert and remove spreadsheets
     doc.sheets.insert_spreadsheet("Test1", 0)
     doc.sheets.insert_spreadsheet("Test2", 2)
     doc.sheets.remove_spreadsheet("Test2")
 
+    # Get sheet by index and set and get cell value
     sheet = doc.sheets.sheet(0)
     sheet.set_cell_value_by_index("value1", 1, 0,)
     print (sheet.cell_value_by_index(1, 0))
 
+    # Get sheet by name and set and get cell value
     sheet = doc.sheets.sheet("Test1")
     sheet.set_cell_value_by_index("value2", 0, 1,)
     print (sheet.cell_value_by_index(0, 1))
-
-    del doc
