2184bec1c8c04956b7350366a51a910bab17f814
Yuriy Puchkov <yuriy.puchkov@namecheap.com>
2020-03-11 08:16:59 +0200

    Django Tutorial-04

--- django-01/mysite/polls/admin.py
+++ django-01/mysite/polls/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
+
 
 admin.site.register(Question)
+
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_filter = ('question__question_text',)
+    list_display = ('question', 'choice_text', 'votes', )
+
--- django-01/mysite/polls/urls.py
+++ django-01/mysite/polls/urls.py
@@ -4,12 +4,8 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    # ex: /polls/
-    path('', views.index, name='index'),
-    # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
