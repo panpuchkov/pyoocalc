f7c31c515fdafab5b0d5d602b3ad25c13d74d176
Yuriy Puchkov <yuriy.puchkov@namecheap.com>
2020-03-25 21:37:29 +0200

    Django Tutorial-07.1

--- django-01/mysite/polls/admin.py
+++ django-01/mysite/polls/admin.py
@@ -3,11 +3,24 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
 
 @admin.register(Choice)
 class ChoiceAdmin(admin.ModelAdmin):
-    list_filter = ('question__question_text',)
+    list_filter = ('question__question_text', 'question__pub_date')
     list_display = ('question', 'choice_text', 'votes', )
+# admin.site.register(Choice, ChoiceAdmin)
 
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+# admin.site.register(Question, QuestionAdmin)
